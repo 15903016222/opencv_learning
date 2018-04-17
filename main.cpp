4ffd4a7e2b8de9b3ddf6636f2cb84666eeab03ee
Wang Sheng <15903016222@163.com>
2018-04-17 20:22:37 +0800

    1-1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
-    printf ("Hello World.\n");
+    Mat srcImage = imread("../1.jpg"); // 载入图像
+    imshow("原始图", srcImage); // 显示图像
+    waitKey(0); // 等待按键按下
+
     return 0;
 }
 
