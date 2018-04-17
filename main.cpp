bf626e9222dbb97e957af8a36415e89e0666966e
Wang Sheng <15903016222@163.com>
2018-04-17 20:43:01 +0800

    1-3,blur---均值滤波---模糊操作

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ using namespace cv;
 
 int main(int argc, char *argv[])
 {
-    Mat srcImage = imread("../erode.jpg"); // 载入图像
+    Mat srcImage = imread("../blur.jpg"); // 载入图像
     imshow("原始图", srcImage); // 显示图像
-    // 进行腐蚀操作
-    Mat element = getStructuringElement(MORPH_RECT, Size (5, 5));
+
+    // 均值滤波操作
     Mat dstImage;
-    erode(srcImage,dstImage, element); // 腐蚀操作函数
-    imshow("效果图---腐蚀操作", dstImage);
+    blur(srcImage, dstImage, Size (7, 7)); // 腐蚀操作函数
+    imshow("效果图---均值滤波", dstImage);
 
     waitKey(0); // 等待按键按下
 
