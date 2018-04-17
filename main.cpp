7889a4b7520a9042ef73edc111f9a1f85a308ced
Wang Sheng <15903016222@163.com>
2018-04-18 00:18:46 +0800

    1-6，保存摄像头数据为视频

--- main.cpp
+++ main.cpp
@@ -1,34 +1,67 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
+/**
+  @file videowriter_basic.cpp
+  @brief A very basic sample for using VideoWriter and VideoCapture
+  @author PkLab.net
+  @date Aug 24, 2016
+*/
+
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
 #include <stdio.h>
+
 using namespace cv;
+using namespace std;
 
-int main(int argc, char *argv[])
+int main(int, char**)
 {
-    VideoCapture capture(0);
-    Mat edges;
+    Mat src;
+    // use default camera as video source
+    VideoCapture cap(0);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+    // get one frame from camera to know frame size and type
+    cap >> src;
+    // check if we succeeded
+    if (src.empty()) {
+        cerr << "ERROR! blank frame grabbed\n";
+        return -1;
+    }
+    bool isColor = (src.type() == CV_8UC3);
 
-    while (1)
-    {
-        Mat frame;
-        capture >> frame;
-        if (frame.empty()) {
-            break;
-        }
-
-        cvtColor(frame, edges, CV_RGB2GRAY);
-
-        blur(edges, edges, Size(3, 3));
-
-        Canny(edges, edges, 0, 30, 3);
-
-        imshow("摄像头", edges);
-        if (waitKey(20) == 27)//27是键盘摁下esc时，计算机接收到的ascii码值
-        {
-            break;
-        }
+    //--- INITIALIZE VIDEOWRITER
+    VideoWriter writer;
+    int codec = VideoWriter::fourcc('M', 'J', 'P', 'G');  // select desired codec (must be available at runtime)
+    double fps = 25.0;                          // framerate of the created video stream
+    string filename = "./live.avi";             // name of the output video file
+    writer.open(filename, codec, fps, src.size(), isColor);
+    // check if we succeeded
+    if (!writer.isOpened()) {
+        cerr << "Could not open the output video file for write\n";
+        return -1;
     }
 
+    //--- GRAB AND WRITE LOOP
+    cout << "Writing videofile: " << filename << endl
+         << "Press any key to terminate" << endl;
+    for (;;)
+    {
+        // check if we succeeded
+        if (!cap.read(src)) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        // encode the frame into the videofile stream
+        writer.write(src);
+        // show live and wait for a key with timeout long enough to show images
+        imshow("Live", src);
+        if (waitKey(5) >= 0)
+            break;
+    }
+    // the videofile will be closed and released automatically in VideoWriter destructor
     return 0;
 }
