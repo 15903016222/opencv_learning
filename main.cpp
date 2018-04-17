1492387496dd41c589a89a356d02aefa54e75ba4
Wang Sheng <15903016222@163.com>
2018-04-17 21:46:47 +0800

    1-6，对摄像头的图像进行边缘处理

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <stdio.h>
 using namespace cv;
 
 int main(int argc, char *argv[])
 {
     VideoCapture capture(0);
-
-    Mat frame;
+    Mat edges;
 
     while (1)
     {
+        Mat frame;
         capture >> frame;
         if (frame.empty()) {
             break;
         }
-        imshow("摄像头", frame);
+
+        cvtColor(frame, edges, CV_RGB2GRAY);
+
+        blur(edges, edges, Size(3, 3));
+
+        Canny(edges, edges, 0, 30, 3);
+
+        imshow("摄像头", edges);
         if (waitKey(20) == 27)//27是键盘摁下esc时，计算机接收到的ascii码值
         {
             break;
