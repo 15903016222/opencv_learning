08e48c3d284ae551dd9e2895b35ce477b7649270
Wang Sheng <15903016222@163.com>
2018-04-18 22:21:18 +0800

    4-1，创建Mat矩阵的七种方法

--- main.cpp
+++ main.cpp
@@ -1,49 +1,43 @@
 #include <opencv2/imgcodecs.hpp>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
 
-static void createAlphaMat(Mat &mat)
-{
-    CV_Assert(mat.channels() == 4);
-    for (int i = 0; i < mat.rows; ++i)
-    {
-        for (int j = 0; j < mat.cols; ++j)
-        {
-            Vec4b& bgra = mat.at<Vec4b>(i, j);
-            bgra[0] = UCHAR_MAX; // Blue
-            bgra[1] = saturate_cast<uchar>((float (mat.cols - j)) / ((float)mat.cols) * UCHAR_MAX); // Green
-            bgra[2] = saturate_cast<uchar>((float (mat.rows - i)) / ((float)mat.rows) * UCHAR_MAX); // Red
-            bgra[3] = saturate_cast<uchar>(0.5 * (bgra[1] + bgra[2])); // Alpha
-        }
-    }
-}
-
 int main()
 {
-    // Create mat with alpha channel
-    // 4通道，每个通道的数据类型8bit的uchar类型
-    Mat mat(480, 640, CV_8UC4);
-    createAlphaMat(mat);
-    vector<int> compression_params;
-    // png格式的压缩
-    // 压缩等级
-    compression_params.push_back(IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(9);
+    cout << "显示创建Mat的七种方法：" << endl;
 
-    bool result = false;
-    try
-    {
-        result = imwrite("../alpha.png", mat, compression_params);
-    }
-    catch (const cv::Exception& ex)
-    {
-        fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
-    }
-    if (result)
-        printf("Saved PNG file with alpha data.\n");
-    else
-        printf("ERROR: Can't save PNG file.\n");
-    return result ? 0 : 1;
+    cout << "方法一：" << endl;
+    Mat M(2, 2, CV_8UC3, Scalar(0, 0, 255));
+    cout << "M = " << endl << " " << M << endl;
+
+    cout << "方法二：" << endl;
+    int sz[3] = {2, 2, 2};
+    Mat L(3, sz, CV_8UC(3), Scalar::all(0)); // 创建超过二维的矩阵
+
+    cout << "方法三：" << endl;
+    M.create(4, 4, CV_8UC(2));
+    cout << "M = " << endl << " " << M << endl;
+
+    cout << "方法四：采用Matble的方式创建" << endl;
+    Mat E = Mat::eye(4, 4, CV_64F);
+    cout << "E = " << endl << " " << E << endl;
+
+    Mat O = Mat::ones(4, 4, CV_64F);
+    cout << "O = " << endl << " " << O << endl;
+
+    Mat Z = Mat::zeros(4, 4, CV_64F);
+    cout << "Z = " << endl << " " << Z << endl;
+
+    cout << "方法六：" << endl;
+    Mat C = (Mat_<double> (3, 3) << 0, -1, 0, -1, 5, -1, 0, -1, 0);
+    cout << "C = " << endl << " " << C << endl;
+
+    cout << "方法七：" << endl;
+    Mat RowClone = C.row(1).clone();
+    cout << "RowClone = " << endl << " " << RowClone << endl;
+
+    return 0;
 }
 
