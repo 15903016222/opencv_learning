df6b98dd8e06dbbd46f4d2e18a7eb27bc78eb92c
Wang Sheng <15903016222@163.com>
2018-04-19 21:43:53 +0800

    4-8, 两张图像的权重叠加操作addWeighted()

--- main.cpp
+++ main.cpp
@@ -37,9 +37,53 @@ bool ROI_addImage() {
     return true;
 }
 
+// -------------[LinearBlending()函数]----------
+// 函数名：LinearBlending()
+// 描述：利用addWeighted()函数实现线性混合
+// ---------------------------------------------
+bool linearBlending() {
+    double alphaValue = 0.5;
+    double betaValue = 1.0 - alphaValue;
+
+    Mat srcImage2, srcImage3, dstImage;
+
+    srcImage2 = imread ("../mogu.jpg");
+    srcImage3 = imread ("../rain.jpg");
+
+    if (! srcImage2.data) {
+        cout << "读取srcImage2错误" << endl;
+        return false;
+    }
+    if (! srcImage3.data) {
+        cout << "读取srcImage3错误" << endl;
+        return false;
+    }
+
+    // 权重加载函数详解,函数
+    // dst = src1[I] * alpha + src2[I] * beta + gamma; I:表示多维数组的索引值
+    // 第1个参数：需要加权的第一个数组
+    // 第2个参数：第一个数组的权重
+    // 第3个参数：需要加权的第二个数组
+    // 第4个参数：第二个数组的权重
+    // 第5个参数：需要加权的总和上的标量值
+    // 第6个参数：需要输出的数组
+    // 第7个参数：默认值-1，即等同于src1.depth()，表示两个图像的大小，深度相同
+    addWeighted(srcImage2, alphaValue, srcImage3, betaValue, 0.0, dstImage);
+
+    namedWindow("线性混合【原图】");
+    imshow("线性混合【原图】", srcImage2);
+
+    namedWindow("线性混合【效果图】");
+    imshow("线性混合【效果图】", dstImage);
+
+    return true;
+}
+
 int main (int argc, char *argv[]) {
 
     ROI_addImage();
+
+    linearBlending();
 
     waitKey(0);
 
