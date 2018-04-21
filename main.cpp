e0fd030b3e282fc48be491862b9b24a75d34a32b
Wang Sheng <15903016222@163.com>
2018-04-21 11:08:34 +0800

    4-17, 三种线性滤波的综合示例，高斯滤波的内核大小必须是奇数

--- main.cpp
+++ main.cpp
@@ -7,56 +7,62 @@
 using namespace cv;
 using namespace std;
 
+// 全局变量声明
+// 原始图像 创建3个不同线性滤波的输出图像
+Mat srcImage, g_dstBoxFilter, g_dstBlur, g_dstGaussionBlur;
+int g_nBoxFilter = 3;       // 方框滤波内核参数大小
+int g_nBlur = 3;            // 均值滤波内核参数大小
+int g_nGaussionBlur = 3;    // 高斯滤波内核参数大小
+
+// 3个轨迹条的回调函数
+void on_BoxFilter (int , void *) {
+    boxFilter(srcImage,
+              g_dstBoxFilter,
+              -1,
+              Size(g_nBoxFilter + 1, g_nBoxFilter + 1)
+              /*, Point(-1, -1), true, BORDER_DEFAULT*/); // 注释的都是默认值
+
+    imshow("方框滤波【效果图】", g_dstBoxFilter);
+}
+
+void on_Blur (int , void *) {
+    blur(srcImage, g_dstBlur,
+         Size(g_nBlur + 1, g_nBlur + 1)
+         /*, Point(-1, -1), BORDER_DEFAULT*/); // 注释的都是默认值
+
+    imshow("均值滤波【效果图】", g_dstBlur);
+}
+
+void on_GaussionBlur (int , void *) {
+    GaussianBlur(srcImage,
+                 g_dstGaussionBlur,
+                 Size(g_nGaussionBlur << 1 | 1, g_nGaussionBlur << 1 | 1), // 高斯滤波的内核大小必须是奇数
+                 0,
+                 0/*, BORDER_DEFAULT*/);
+
+    imshow("高斯滤波【效果图】", g_dstGaussionBlur);
+}
+
 int main (int argc, char *argv[]) {
-    // 创建原图窗口
+    // 创建原图窗口 3个滤波窗口
     namedWindow("【原图】");
+    namedWindow("方框滤波【效果图】");
+    namedWindow("均值滤波【效果图】");
+    namedWindow("高斯滤波【效果图】");
+
     // 读入图像,显示原图
-    Mat srcImage = imread ("../boxfilter.jpg");
+    srcImage = imread ("../boxfilter.jpg");
     imshow("【原图】", srcImage);
 
-    /* --------------方波滤波【boxFilter()】 */
-    // 方波滤波操作
-    Mat dstImage;
-    // 第一个参数： 原始输入图像
-    // 第二个参数： 输出图像
-    // 第三个参数： ddepth,输出图像深度，-1：表示使用的原始输入图像的深度
-    // 第四个参数： 方波滤波的内核大小(方波大小)
-    // 第五个参数： Point(w, h), 表示方波的中心：(-1, -1) 并且还是默认值
-    // 第六个参数： normalize:是否矩阵归一化，默认值true
-    // 第七个参数： borderType:一般不需要设置，使用默认值即可
-    boxFilter(srcImage, dstImage, -1, Size(3, 3)/*, Point(-1, -1), true, BORDER_DEFAULT*/);
+    // 创建3个轨迹条
+    createTrackbar("方框滤波", "方框滤波【效果图】", &g_nBoxFilter, 40, on_BoxFilter, NULL);
+    createTrackbar("均值滤波", "均值滤波【效果图】", &g_nBlur, 40, on_Blur, NULL);
+    createTrackbar("高斯滤波", "高斯滤波【效果图】", &g_nGaussionBlur, 40, on_GaussionBlur, NULL);
 
-    // 显示效果图
-    namedWindow("方波滤波【效果图】");
-    imshow("方波滤波【效果图】", dstImage);
-
-    /* ---------均值滤波[blur()]-------------- */
-    Mat blurImage;
-    // 第一个参数： 原始输入图像
-    // 第二个参数： 输出图像
-    // 第三个参数： 内核大小
-    // 第四个参数： 表示锚点，表示需要平滑的那个点
-    // 第五个参数： 一般使用默认的，不要太关心
-    blur(srcImage, blurImage, Size(3, 3), Point(-1, -1)/*, BORDER_DEFAULT*/);
-
-    // 显示均值滤波
-    namedWindow("均值滤波【效果图】");
-    imshow("均值滤波【效果图】", blurImage);
-
-    /* -------------高斯滤波GaussionBlur()---------- */
-    Mat gaussionImage;
-    // 第一个参数： 原始输入图像
-    // 第二个参数： 输出图像
-    // 第三个参数： 内核大小
-    // 第四个参数： 高斯函数在X方向的标准偏差
-    // 第五个参数： 高斯函数在Y方向的标准偏差 默认值0
-    //            若X，Y都为0， 那么就由ksize.width ksize.height计算出来
-    // 第六个参数： 使用默认值，一般不用太关心这个参数
-    GaussianBlur(srcImage, gaussionImage, Size(3, 3), 0, 0/*, BORDER_DEFAULT*/);
-
-    // 显示高斯滤波操作
-    namedWindow("高斯滤波【效果图】");
-    imshow("高斯滤波【效果图】", gaussionImage);
+    // 初始化3个轨迹条
+    on_BoxFilter(g_nBoxFilter, NULL);
+    on_Blur(g_nBlur, NULL);
+    on_GaussionBlur(g_nGaussionBlur, NULL);
 
     waitKey(0);
     return 0;
