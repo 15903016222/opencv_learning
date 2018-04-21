4e96e2c302930c5217f7d92ebe1b7d395e3e2a1a
Wang Sheng <15903016222@163.com>
2018-04-21 15:06:18 +0800

    4-21, 图像膨胀操作函数dilate()，示例

--- main.cpp
+++ main.cpp
@@ -7,106 +7,41 @@
 using namespace cv;
 using namespace std;
 
-// 全局变量的定义及描述
-Mat g_srcImage;
-Mat g_dstBoxFilterImgage, g_dstBlurImage, g_dstGaussionImage;
-Mat g_dstMediaImage, g_dstbilateraImage;
-
-int g_nBoxFilter = 4;
-int g_nBlur = 4;
-int g_nGaussion = 4;
-int g_nMedia = 4;
-int g_nBilatera =4;
-
-// 轨迹回调函数
-void on_BoxFilter (int , void *) {
-    boxFilter(g_srcImage,
-              g_dstBoxFilterImgage,
-              -1,
-              Size(g_nBoxFilter + 1, g_nBoxFilter + 1));
-    imshow("<1>方框滤波【效果图】", g_dstBoxFilterImgage);
-}
-
-void on_Blur (int , void *) {
-    blur(g_srcImage,
-         g_dstBlurImage,
-         Size(g_nBlur + 1, g_nBlur + 1));
-    imshow("<2>均值滤波【效果图】", g_dstBlurImage);
-}
-
-void on_Guassion (int , void *) {
-    GaussianBlur(g_srcImage,
-                 g_dstGaussionImage,
-                 Size(g_nGaussion << 2 | 1, g_nGaussion << 2 | 1), // Size 必须是奇数
-                 0, 0);
-
-    imshow("<3>高斯滤波【效果图】", g_dstGaussionImage);
-}
-
-void on_Media (int , void *) {
-    medianBlur(g_srcImage,
-               g_dstMediaImage,
-               g_nMedia << 2 | 1);  // 这个参数必须是大于1的奇数
-
-    imshow("<4>中值滤波【效果图】", g_dstMediaImage);
-}
-
-void on_Bilatera (int , void *) {
-    bilateralFilter(g_srcImage,
-                    g_dstbilateraImage,
-                    g_nBilatera,
-                    g_nBilatera << 1,
-                    g_nBilatera >> 1);
-
-    imshow("<5>双边滤波【效果图】", g_dstbilateraImage);
-}
-
 int main (int argc, char *argv[]) {
     // 创建窗口
     namedWindow("<0>原始图像");
     // 读取原始图像，并显示
-    g_srcImage = imread("../filter.jpg");
-    imshow("<0>原始图像", g_srcImage);
+    Mat srcImage = imread("../dilate.jpg");
+    imshow("<0>原始图像", srcImage);
 
-    /* -----------方框滤波--------- */
-    // 1. 创建方框滤波效果窗口
-    namedWindow("<1>方框滤波【效果图】");
-    // 2. 创建轨迹条
-    createTrackbar("方框滤波", "<1>方框滤波【效果图】", &g_nBoxFilter, 40, on_BoxFilter, NULL);
-    // 3. 初始化轨迹条
-    on_BoxFilter(g_nBoxFilter, NULL);
-
-    /* -----------均值滤波--------- */
-    // 1. 创建均值滤波效果窗口
-    namedWindow("<2>均值滤波【效果图】");
-    // 2. 创建轨迹条
-    createTrackbar("均值滤波", "<2>均值滤波【效果图】", &g_nBlur, 40, on_Blur);
-    // 3. 初始化轨迹条
-    on_Blur(g_nBlur, NULL);
-
-    /* -----------高斯滤波--------- */
-    // 1. 创建高斯滤波效果窗口
-    namedWindow("<3>高斯滤波【效果图】");
-    // 2. 创建轨迹条
-    createTrackbar("高斯滤波", "<3>高斯滤波【效果图】", &g_nGaussion, 40, on_Guassion);
-    // 3. 初始化轨迹条
-    on_Guassion(g_nGaussion, NULL);
-
-    /* -----------中值滤波--------- */
-    // 1. 创建中值滤波效果窗口
-    namedWindow("<4>中值滤波【效果图】");
-    // 2. 创建轨迹条
-    createTrackbar("中值滤波", "<4>中值滤波【效果图】", &g_nMedia, 40, on_Media);
-    // 3. 初始化轨迹条
-    on_Media(g_nMedia, NULL);
-
-    /* -----------双边滤波--------- */
-    // 1. 创建双边滤波效果窗口
-    namedWindow("<5>双边滤波【效果图】");
-    // 2. 创建轨迹条
-    createTrackbar("双边滤波", "<5>双边滤波【效果图】", &g_nBilatera, 40, on_Bilatera);
-    // 3. 初始化轨迹条
-    on_Bilatera(g_nBilatera, NULL);
+    /* ------------膨胀操作dilate()------------- */
+    namedWindow("<1>膨胀操作【效果图】");
+    Mat dilateImage;
+    Mat element;
+    // 获取自定义核
+    // 描述：获取自定义核的信息
+    // 第一个参数： 自定义核的形状
+    // 第二个参数： 自定义核的大小
+    // 第三个参数： 锚点位置
+    element = getStructuringElement(MORPH_RECT, Size(15, 15), Point(-1, -1));
+    // 膨胀操作
+    // 描述：对图像进行膨胀操作
+    // 第一个参数： 原始输入图像
+    // 第二个参数： 输出图像
+    // 第三个参数： 自定义核
+    // 第四个参数： 锚点 默认值Point(-1, -1)
+    // 第五个参数： 迭代使用dilate()函数的次数，默认值1
+    // 第六个参数： 用于推断图像外部像素的某种边界模式，默认值：BORDER_CONSTANT
+    // 第七个参数： 一般不用特别关注，如果用到，需要到文档中仔细阅读
+    dilate(srcImage,
+           dilateImage,
+           element,
+           Point(-1, -1),
+           1,
+           BORDER_CONSTANT,
+           morphologyDefaultBorderValue());
+    // 显示效果图
+    imshow("<1>膨胀操作【效果图】", dilateImage);
 
     waitKey(0);
     return 0;
