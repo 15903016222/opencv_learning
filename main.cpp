dd1e747819d55cdd6990c546e68d95700638371d
Wang Sheng <15903016222@163.com>
2018-04-21 10:00:41 +0800

    4-15, blur()均值滤波函数,使用滤波函数

--- main.cpp
+++ main.cpp
@@ -8,14 +8,13 @@ using namespace cv;
 using namespace std;
 
 int main (int argc, char *argv[]) {
-    /* --------------方波滤波【boxFilter()】 */
     // 创建两个窗口
     namedWindow("方波滤波【原图】");
-    namedWindow("方波滤波【效果图】");
     // 读入图像,显示原图
     Mat srcImage = imread ("../boxfilter.jpg");
     imshow("方波滤波【原图】", srcImage);
 
+    /* --------------方波滤波【boxFilter()】 */
     // 方波滤波操作
     Mat dstImage;
     // 第一个参数： 原始输入图像
@@ -28,7 +27,21 @@ int main (int argc, char *argv[]) {
     boxFilter(srcImage, dstImage, -1, Size(3, 3)/*, Point(-1, -1), true, BORDER_DEFAULT*/);
 
     // 显示效果图
+    namedWindow("方波滤波【效果图】");
     imshow("方波滤波【效果图】", dstImage);
+
+    /* ---------均值滤波[blur()]-------------- */
+    Mat blurImage;
+    // 第一个参数： 原始输入图像
+    // 第二个参数： 输出图像
+    // 第三个参数： 内核大小
+    // 第四个参数： 表示锚点，表示需要平滑的那个点
+    // 第五个参数： 一般使用默认的，不要太关心
+    blur(srcImage, blurImage, Size(3, 3), Point(-1, -1)/*, BORDER_DEFAULT*/);
+
+    // 显示均值滤波
+    namedWindow("均值滤波【效果图】");
+    imshow("均值滤波【效果图】", blurImage);
 
     waitKey(0);
     return 0;
