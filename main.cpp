8be26fe66e947400f48606117529865d1d2ce3d7
Wang Sheng <15903016222@163.com>
2018-04-21 15:16:50 +0800

    4-22，图像腐蚀操作函数erode()， 示例

--- main.cpp
+++ main.cpp
@@ -43,6 +43,33 @@ int main (int argc, char *argv[]) {
     // 显示效果图
     imshow("<1>膨胀操作【效果图】", dilateImage);
 
+    /* -------------腐蚀操作erode()------------- */
+    namedWindow("<2>腐蚀操作【原图】");
+    // 读取原始图像
+    Mat srcErodeImage = imread("../erode.jpg");
+    imshow("<2>腐蚀操作【原图】", srcErodeImage);
+
+    // 腐蚀操作
+    namedWindow("<3>腐蚀操作【效果图】");
+    Mat dstErodeImage;
+    // 腐蚀操作
+    // 描述：对图像进行腐蚀操作
+    // 第一个参数： 原始输入图像
+    // 第二个参数： 输出图像
+    // 第三个参数： 自定义核
+    // 第四个参数： 锚点 默认值Point(-1, -1)
+    // 第五个参数： 迭代使用dilate()函数的次数，默认值1
+    // 第六个参数： 用于推断图像外部像素的某种边界模式，默认值：BORDER_CONSTANT
+    // 第七个参数： 一般不用特别关注，如果用到，需要到文档中仔细阅读
+    erode(srcErodeImage,
+          dstErodeImage,
+          element,
+          Point(-1, -1),
+          1,
+          BORDER_CONSTANT,
+          morphologyDefaultBorderValue());
+    imshow("<3>腐蚀操作【效果图】", dstErodeImage);
+
     waitKey(0);
     return 0;
 }
