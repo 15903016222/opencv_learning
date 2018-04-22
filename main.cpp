c0e3fee4ff8d40cb2100f433e6d22620c344270f
Wang Sheng <15903016222@163.com>
2018-04-22 22:49:12 +0800

    4-33, 边缘检测Sobel()算子， 示例

--- main.cpp
+++ main.cpp
@@ -10,35 +10,64 @@ int main( int argc, char** argv )
 {
     // 创建窗口 读取图片 显示原图
     namedWindow("<0>【原图】");
-    Mat srcImage = imread ("../Canny.jpg");
+    Mat srcImage = imread ("../sobel.jpg");
     imshow("<0>【原图】", srcImage);
 
-    // 高阶Canny()用法，转成灰度图，降噪，用canny，最后将得到的边缘最为掩码
-    // 拷贝原图的效果上，得到彩色的边缘图
-    Mat srcImage1 = srcImage.clone();
+    // 创建gray_x gray_y矩阵
+    Mat gray_x, gray_y;
+    Mat abs_gray_x, abs_gray_y, dstImage;
 
-    Mat dstImage, edgeImage, grayImage;
-    // 创建与src同类型和大小的矩阵【dstImage】
-    dstImage.create(srcImage1.size(), srcImage1.type());
-    // 将原图像转换为灰度图
-    cvtColor(srcImage1, grayImage, COLOR_BGR2GRAY);
-    // 均值滤波3x3内核滤波
-    blur(grayImage, edgeImage, Size(3, 3));
-    // 运行Canny算子
+    // 求 x 方向梯度
+    // 描述：用于边缘检测的离散微分算子，利用高斯平滑和微分求导
     // 第一个参数： 原始输入图像
     // 第二个参数： 输出图像
-    // 第三个参数： 第一个滞后阀值
-    // 第四个参数： 第二个滞后阀值
-    // 第五个参数： 应用Sobel算子的孔径大小 默认值3
-    // 第六个参数： 图像梯度幅度的标识 默认值false
-    Canny(edgeImage, edgeImage, 3, 9, 3);
-    // 将dstImage内的所有元素设置为0
-    dstImage = Scalar::all(0);
-    // 将获得的掩码，来将原图拷贝到输出图像
-    srcImage1.copyTo(dstImage, edgeImage);
+    // 第三个参数： 输出图像深度
+    // 第四个参数： X方向差分阶数
+    // 第五个参数： Y方向差分阶数
+    // 第六个参数： Sobel核的大小，必须是1，3， 5或7
+    // 第七个参数： 计算导数值时可选的缩放因子，默认值1，表示默认情况下是没有应用缩放
+    // 第八个参数： 结果存入目标图之前可选的delta值 默认值0
+    // 第九个参数： 不用特别关心，使用默认值即可
+    Sobel(srcImage,
+          gray_x,
+          CV_16S,
+          1,
+          0,
+          3,
+          1,
+          0,
+          BORDER_DEFAULT);
+    // 先缩放元素再取绝对值，最后转换格式为8bit型
+    convertScaleAbs(gray_x, abs_gray_x);
+    imshow("<1>显示X方向的Sobel", abs_gray_x);
 
-    imshow("<1>【效果图】", dstImage);
-    imshow("<2>【掩码图】", edgeImage);
+    // 求 y 方向梯度
+    // 描述：用于边缘检测的离散微分算子，利用高斯平滑和微分求导
+    // 第一个参数： 原始输入图像
+    // 第二个参数： 输出图像
+    // 第三个参数： 输出图像深度
+    // 第四个参数： X方向差分阶数
+    // 第五个参数： Y方向差分阶数
+    // 第六个参数： Sobel核的大小，必须是1，3， 5或7
+    // 第七个参数： 计算导数值时可选的缩放因子，默认值1，表示默认情况下是没有应用缩放
+    // 第八个参数： 结果存入目标图之前可选的delta值 默认值0
+    // 第九个参数： 不用特别关心，使用默认值即可
+    Sobel(srcImage,
+          gray_y,
+          CV_16S,
+          0,
+          1,
+          3,
+          1,
+          0,
+          BORDER_DEFAULT);
+    // 先缩放元素再取绝对值，最后转换格式为8bit型
+    convertScaleAbs(gray_y, abs_gray_y);
+    imshow("<2>显示Y方向的Sobel", abs_gray_y);
+
+    // 合并梯度(近似)
+    addWeighted(abs_gray_x, 0.5, abs_gray_y, 0.5, 0, dstImage);
+    imshow("<3>【效果图】", dstImage);
 
     waitKey(0);
 
