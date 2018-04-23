36c517bf484a17d648f1dd0df024d550505d7f75
Wang Sheng <15903016222@163.com>
2018-04-23 20:51:32 +0800

    4-36, 边缘检测综合实例，Canny()/Sobel()/Scharr(), 应用

--- main.cpp
+++ main.cpp
@@ -6,51 +6,82 @@
 using namespace cv;
 using namespace std;
 
+// 定义全局变量
+Mat g_srcImage, g_srcGrayImage, g_dstImage;
+
+Mat g_cannyDetecteEdges;
+int g_cannyLowThreshold = 1;
+
+Mat g_sobelGradient_X, g_sobelGradient_Y;
+Mat g_sobelAbsGradient_X, g_sobelAbsGradient_Y;
+int g_sobelKernelSize = 1;
+
+Mat g_scharrGradient_X, g_scharrGradient_Y;
+Mat g_scharrAbsGradient_X, g_scharrAbsGradient_Y;
+
+// 回调函数
+void on_Canny (int , void *) {
+    // 滤波
+    blur(g_srcGrayImage, g_cannyDetecteEdges, Size(3, 3));
+
+    // 运行Canny算子
+    Canny(g_cannyDetecteEdges, g_cannyDetecteEdges, g_cannyLowThreshold, g_cannyLowThreshold * 3, 3);
+
+    g_dstImage = Scalar::all(0);
+    g_srcImage.copyTo(g_dstImage, g_cannyDetecteEdges);
+
+    // 显示
+    imshow("<1>Canny【效果图】", g_dstImage);
+}
+
+void on_Sobel (int , void *) {
+    // Sobel算子
+    Sobel(g_srcImage, g_sobelGradient_X, CV_16S, 1, 0, (g_sobelKernelSize << 1 | 1), 1, 0);
+    convertScaleAbs(g_sobelGradient_X, g_sobelAbsGradient_X);
+
+    Sobel(g_srcImage, g_sobelGradient_Y, CV_16S, 0, 1, (g_sobelKernelSize << 1 | 1), 1, 0);
+    convertScaleAbs(g_sobelGradient_Y, g_sobelAbsGradient_Y);
+
+    addWeighted(g_sobelAbsGradient_X, 0.5, g_sobelAbsGradient_Y, 0.5, 0, g_dstImage);
+
+    // 显示
+    imshow("<2>Sobel【效果图】", g_dstImage);
+}
+
+void Scharr1 () {
+    // 求X方向的梯度
+    Scharr(g_srcImage, g_scharrGradient_X, CV_16S, 1, 0, 1, 0);
+    convertScaleAbs(g_scharrGradient_X, g_scharrAbsGradient_X);
+
+    // 求Y方向的梯度
+    Scharr(g_srcImage, g_scharrGradient_Y, CV_16S, 0, 1, 1, 0);
+    convertScaleAbs(g_scharrGradient_Y, g_scharrAbsGradient_Y);
+
+    // 叠加
+    addWeighted(g_scharrAbsGradient_X, 0.5, g_scharrAbsGradient_Y, 0.5, 0, g_dstImage);
+
+    // 显示
+    imshow("<3>Scharr【效果图】", g_dstImage);
+}
+
 int main( int argc, char** argv )
 {
-    // 创建窗口 读取图片 显示原图
     namedWindow("<0>【原图】");
-    Mat srcImage = imread ("../scharr.jpg");
-    imshow("<0>【原图】", srcImage);
+    g_srcImage = imread ("../edges.jpg");
+    imshow("<0>【原图】", g_srcImage);
 
-    Mat grad_x, grad_y;
-    Mat abs_grad_x, abs_grad_y, dst;
+    g_dstImage.create(g_srcImage.size(), g_srcImage.type());
+    cvtColor(g_srcImage, g_srcGrayImage, COLOR_BGR2GRAY);
 
-    // 求X方向梯度
-    // 描述： Schaar()算子 边缘检测
-    // 第一个参数： 原始输入图像
-    // 第二个参数： 输出图像
-    // 第三个参数： 输出图像深度
-    // 第四个参数： X方向差分阶数
-    // 第五个参数： Y方向差分阶数
-    // 第六个参数： 计算导数时可选的缩放因子
-    // 第七个参数： 结果存入目标图之前可选的delta的值 默认值0
-    // 第八个参数： 不用关心，需要用时需要修改参数
-    Scharr(srcImage,
-           grad_x,
-           CV_16S,
-           1,
-           0,
-           1,
-           0,
-           BORDER_DEFAULT);
-    convertScaleAbs(grad_x, abs_grad_x);
+    namedWindow("<1>Canny【效果图】");
+    namedWindow("<2>Sobel【效果图】");
 
-    // 求Y方向梯度
-    Scharr(srcImage,
-           grad_y,
-           CV_16S,
-           0,
-           1,
-           1,
-           0,
-           BORDER_DEFAULT);
-    convertScaleAbs(grad_y, abs_grad_y);
+    createTrackbar("参数值", "<1>Canny【效果图】", &g_cannyLowThreshold, 120, on_Canny);
+    createTrackbar("参数值", "<2>Sobel【效果图】", &g_sobelKernelSize, 3, on_Sobel);
 
-    // 合并梯度
-    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
-
-    imshow("<1>【效果图】", dst);
+    on_Canny(0, NULL);
+    on_Sobel(0, NULL);
+    Scharr1();
 
     waitKey(0);
 
