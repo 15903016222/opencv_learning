91b126eff82ab0068bed89f364c154ffa5f59807
Wang Sheng <15903016222@163.com>
2018-04-24 23:04:19 +0800

    4-48, 累计概率霍夫变换HoughLinesP(), 示例

--- main.cpp
+++ main.cpp
@@ -24,44 +24,28 @@ int main( int argc, char** argv )
     cvtColor(tmpImage, dstImage, COLOR_GRAY2BGR);
 
     // 进行霍夫变换
-    vector<Vec2f> lines;
+    vector<Vec4i> lines;
     // 描述：标准霍夫变换
     // 第一个参数： 输入图像
-    // 第二个参数： 用于存放霍夫变换检测到的线条输出矢量 每条线条有两个参数(rho, theta),rho离原点坐标的距离
-    //                                                                       theta弧度线条转换的角度
+    // 第二个参数： 用于存放霍夫变换检测到的线条输出矢量 每条线条有四个参数(x_1, y_1, x_2, y_2) 直线的两个点
     // 第三个参数： 以像素为单位的距离精度，
     // 第四个参数： 以弧度为单位的角度京精度
     // 第五个参数： 累加平面的阀值参数，即识别某一条直线时，他在累加平面中必须达到的值
-    // 第六个参数： 默认值0
-    // 第七个参数： 默认值0
-    HoughLines(tmpImage,
+    // 第六个参数： 默认值0 最低线段的长度， 比这个长度小的不显示出来
+    // 第七个参数： 默认值0 允许同一行，点与点之间连接起来的最大距离
+    HoughLinesP(tmpImage,
                lines,
                1,
                CV_PI / 180,
-               300,
-               0,
-               0,
-               0);
+               200,
+               50,
+               10);
 
     size_t i;
     for (i = 0; i < lines.size(); ++i) {
-        float rho = lines[i][0];
-        float theta = lines[i][1];
-        Point pt1, pt2;
-        double a = cos (theta);
-        double b = sin (theta);
-        double x0 = a * rho;
-        double y0 = b * rho;
+        Vec4i l = lines[i];
 
-        pt1.x = cvRound(x0 + 1000 * (-b));
-        pt1.y = cvRound(y0 + 1000 * (a));
-        pt2.x = cvRound(x0 - 1000 * (-b));
-        pt2.y = cvRound(y0 - 1000 * (a));
-
-        cout << "pt1(" << pt1.x << ", " << pt1.y << ")" << endl;
-        cout << "pt2(" << pt2.x << ", " << pt2.y << ")" << endl;
-
-        line(dstImage, pt1, pt2, Scalar(55, 100, 195), 1, LINE_AA);
+        line(dstImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(55, 100, 195), 1, LINE_AA);
     }
     cout << "i = " << i << endl;
 
