703d0cb12c5266eb1b0e416599a00f5bf7099698
Wang Sheng <15903016222@163.com>
2018-04-26 19:40:44 +0800

    添加：重映射操作remap()的第三个参数，第四个参数的数据类型

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@ void on_Remap (int , void *) {
         for (int i = 0; i < g_srcImage.cols; ++i) {
             switch (g_ndirection) {
             case 0:
-                // 原图 必须是float型，因为rows和cols的数量会超过255,
                 // 因为这个g_map_x, g_map_y只表示坐标位置信息
                 g_map_x.at<float>(j, i) = static_cast<float>(i);
                 g_map_y.at<float>(j, i) = static_cast<float>(j);
@@ -44,8 +43,10 @@ void on_Remap (int , void *) {
     // 描述： 根据映射的方式，将图像进行重映射几何变换
     // 第一个参数： 输入图像
     // 第二个参数： 输出图像
-    // 第三个参数： 表示点(x, y)的一个映射
-    // 第四个参数： 与上一个相同
+    // 第三个参数： 表示点(x, y)的一个映射,
+    //            数据类型只能是CV_16SC2, CV_32FC1, CV_32FC2类型的X值
+    // 第四个参数： 表示点(x, y)的一个映射
+    //            数据类型只能是CV_16UC1, CV_32FC1类型的X值
     // 第五个参数： 插值方式 INTER_LINEAR双线型插值
     // 第六个参数： 边界模式
     // 第七个参数： 使用默认值Scalar(0, 0, 0)
