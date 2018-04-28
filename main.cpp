27d72c029b31b0dd8c22a4fe735eb413b9b693aa
wangsheng <wangsheng@cndoppler.cn>
2018-04-28 11:16:31 +0800

    批量修改文件的大小为统一尺寸136x36

--- main.cpp
+++ main.cpp
@@ -13,12 +13,8 @@ using namespace cv;
 using namespace std;
 using namespace ml;
 
-vector<Mat> trainingImages; //用来存放训练图像信息的容器
-vector<int> trainingLabels; //用来存放图像对应正负样本的值，正样本为1，负样本为0
-
 int openfile(int flag, const char *dpath)
 {//获取图像的路径
-    vector<string> files;
     DIR *dir;
     struct dirent *ptr;
     if((dir = opendir(dpath)) == NULL)//打开文件夹
@@ -26,62 +22,38 @@ int openfile(int flag, const char *dpath)
         cout << __LINE__ << endl;
         return -1;
     }
+    int count = 0;
     while((ptr = readdir(dir)) != NULL)
     {
         if(ptr->d_type > 4) //过滤. 和 ..
         {
-            char name[30];
+            char name[200];
             char path[200];
             sprintf(path, "%s/%s", dpath, ptr->d_name); //将文件夹路径和图像名结合起来得到图像路径
-            files.push_back(path);
-            puts(path);
+            sprintf(name, "%s/ws/%04d.jpg", dpath, count++);
+            Mat srcImage = imread(path);
+            if (36 == srcImage.rows) {
+                if (136 == srcImage.cols) {
+                    continue;
+                }
+            }
+            printf ("%s \n", path);
+            printf ("%s \n", name);
+            Mat dstImage;
+            resize(srcImage, dstImage, Size(136, 36));
+            imwrite(name, dstImage);
+            cout << __LINE__ << endl;
         }
     }
     closedir(dir);
-    //将图像对应的值存放到容器中
-    for (int i = 0; i < files.size(); i++)
-    {
-        Mat img = imread(files[i].c_str());
-        Mat line_i = img.reshape(1, 1);
-
-        trainingImages.push_back(line_i);
-        trainingLabels.push_back(flag);
-    }
 
     return 0;
 }
 
 int main()
 {
-    Mat classes;
-    openfile(1, "/home/ws/tmp/opencv_learning/svm/posPlate/train");
-    openfile(0, "/home/ws/tmp/opencv_learning/svm/negPlate/train");
-    cout << __LINE__ << endl;
-    Mat trainingData(trainingImages.size(), trainingImages[0].cols, CV_32FC1);
-    for (int i = 0; i < trainingImages.size(); i++)
-    {
-        Mat temp(trainingImages[i]);
-        temp.copyTo(trainingData.row(i));
-    }
-    trainingData.convertTo(trainingData, CV_32FC1);
-    Mat(trainingLabels).copyTo(classes);
-    classes.convertTo(classes, CV_32SC1);
-
-    Ptr<SVM> model = SVM::create();//以下是设置SVM训练模型的配置
-    model->setType(SVM::C_SVC);
-    model->setKernel(SVM::LINEAR);
-    model->setGamma(1);
-    model->setC(1);
-    model->setCoef0(0);
-    model->setNu(0);
-    model->setP(0);
-    model->setTermCriteria(cvTermCriteria(CV_TERMCRIT_ITER, 20000, 0.0001));
-
-
-    Ptr<TrainData> tdata = TrainData::create(trainingData, ROW_SAMPLE, classes);
-    //model->train(trainingData, ROW_SAMPLE, classes);
-    model->train(tdata);
-    model->save("car.xml");//保存
+    openfile(1, "/home/ws/tmp/opencv_learning/svm/posPlate/train/ws");
+    openfile(0, "/home/ws/tmp/opencv_learning/svm/negPlate/train/ws");
 
     return 0;
 }
