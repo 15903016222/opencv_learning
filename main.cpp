f8aaace26cfc7ed35c62f7ca5517c6ebd1120a1a
Wang Sheng <15903016222@163.com>
2018-04-29 15:05:27 +0800

    4-58,绘制多边形边界操作approxPloyDp()，示例

--- main.cpp
+++ main.cpp
@@ -7,68 +7,74 @@ using namespace cv;
 using namespace std;
 
 #define WINDOW_NAME1 "【原始图】"
-#define WINDOW_NAME2 "【轮廓图】"
+#define WINDOW_NAME2 "【效果图】"
+
+// 定义全局变量信息
+Mat g_srcImage;
+Mat g_grayImage;
+int g_nThresh = 50;     // 阀值
+int g_nMaxThresh = 255; // 最大阀值
+RNG g_rng (12345);
+
+// 回调函数
+void on_ContoursChange(int , void *) {
+    // 定义了参数
+    Mat threshold_output;
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    // 图片二值化
+    threshold(g_grayImage, threshold_output, g_nThresh, 255, THRESH_BINARY);
+    // 找出轮廓
+    findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+    // 多边形逼近轮廓 + 获取矩形和圆形边界框
+    vector<vector<Point> > contours_poly(contours.size());
+    vector<Rect> boundRect(contours.size());
+    vector<Point2f> centor(contours.size());
+    vector<float> radius(contours.size());
+
+    // 循环遍历所有部分，进行本程序最核心的操作
+    for (unsigned i = 0; i < contours.size(); ++i) {
+        // 用指定精度逼近多边形曲线
+        approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+        // 计算点集最外面的矩形边界
+        boundRect[i] = boundingRect(Mat(contours_poly[i]));
+        // 对给定的2D点集，寻找最小面积的包围圆形
+        minEnclosingCircle(contours_poly[i], centor[i], radius[i]);
+    }
+
+    // 绘制多边形轮廓 + 包围的矩形 + 圆形框
+    Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
+    for (unsigned int i = 0; i < contours.size(); ++i) {
+        Scalar color = Scalar(g_rng.uniform(0, 255),
+                              g_rng.uniform(0, 255),
+                              g_rng.uniform(0, 255));
+        drawContours(drawing, contours_poly, i, color, 1, LINE_AA);
+
+        rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, LINE_AA, 0);
+
+        circle(drawing, centor[i], radius[i], color, 2, LINE_AA, 0);
+    }
+
+    namedWindow(WINDOW_NAME2);
+    imshow(WINDOW_NAME2, drawing);
+}
 
 int main( int argc, char** argv )
 {
-    Mat image(600, 600, CV_8UC3);
-    RNG &rng = theRNG();
+    g_srcImage = imread("../sea.jpg", 1);
+    imshow(WINDOW_NAME1, g_srcImage);
 
-    while (1) {
-        // 参数初始化
-        int cout = rng.uniform(3, 103);
-        vector<Point> points;
+    cvtColor(g_srcImage, g_grayImage, COLOR_BGR2GRAY);
+    // 平滑操作
+    blur(g_grayImage, g_grayImage, Size(3, 3));
 
-        // 随机生成点坐标
-        for (int i = 0; i < cout; ++i) {
-            Point point;
+    namedWindow(WINDOW_NAME2);
+    createTrackbar("阀值", WINDOW_NAME1, &g_nThresh, g_nMaxThresh, on_ContoursChange);
 
-            point.x = rng.uniform(image.cols / 4, image.cols * 3 / 4);
-            point.y = rng.uniform(image.rows / 4, image.rows * 3 / 4);
-            points.push_back(point);
-        }
+    on_ContoursChange(g_nThresh, NULL);
 
-        // 对给定的2D点集，寻找最小面积的包围圆
-        Point2f center;
-        float radius = 0;
-        // 描述： 绘制使用最小面积的圆包含点集
-        // 第一个参数： 需要包围的点集
-        // 第二个参数： 需要包围的圆心
-        // 第三个参数： 圆的半径
-        minEnclosingCircle(Mat(points), center, radius);
-
-        // 绘制出随机颜色的点
-        image = Scalar::all(0);
-        for (int i = 0; i < cout; ++i) {
-            circle(image,
-                   points[i],
-                   3,
-                   Scalar(rng.uniform(0, 255),
-                          rng.uniform(0, 255),
-                          rng.uniform(0, 255)),
-                   FILLED,
-                   LINE_AA);
-        }
-
-        // 绘制最小面积的包围矩形
-        circle(image,
-               center,
-               radius,
-               Scalar(rng.uniform(0, 255),
-                      rng.uniform(0, 255),
-                      rng.uniform(0, 255)),
-               2,
-               LINE_AA);
-
-        // 显示窗口
-        imshow(WINDOW_NAME2, image);
-
-        // 退出
-        char key = (char)waitKey();
-        if (27 == key || 'q' == key || 'Q' == key) {
-            break;
-        }
-    }
+    waitKey();
 
     return 0;
 }
