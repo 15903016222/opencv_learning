cdd517feac47211cc8e999ecdabf5d1bf71ac5b6
Wang Sheng <15903016222@163.com>
2018-04-29 13:45:21 +0800

    4-56，凸包，轮廓的查找与绘制综合示例

--- main.cpp
+++ main.cpp
@@ -9,68 +9,67 @@ using namespace std;
 #define WINDOW_NAME1 "【原始图】"
 #define WINDOW_NAME2 "【轮廓图】"
 
+// 描述全局变量
+Mat g_srcImage, g_grayImage;
+int g_nThresh = 50;
+int g_maxThresh = 255;
+RNG g_rng(12345);
+Mat srcImage_copy = g_srcImage.clone();
+Mat g_thresholdImage_output;
+vector<vector<Point> > g_vContours;
+vector<Vec4i> g_vHierarchy;
+
+void on_ThreshChange(int , void *) {
+    // 对图像进行二值化，控制阀值
+    threshold(g_grayImage, g_thresholdImage_output, g_nThresh, 255, THRESH_BINARY);
+    // 寻找轮廓
+    findContours(g_thresholdImage_output,
+                 g_vContours,
+                 g_vHierarchy,
+                 RETR_TREE,
+                 CHAIN_APPROX_SIMPLE);
+    // 遍历每个轮廓
+    vector<vector<Point> > hull(g_vContours.size());
+    for (unsigned int i = 0; i < hull.size(); ++i) {
+        convexHull(Mat(g_vContours[i]), hull[i], false);
+    }
+
+    // 绘制轮廓和凸包
+    Mat drawing = Mat::zeros(g_thresholdImage_output.size(), CV_8UC3);
+    for (int i = 0; i < g_vContours.size(); ++i) {
+        Scalar color = Scalar(g_rng.uniform(0, 255),
+                              g_rng.uniform(0, 255),
+                              g_rng.uniform(0, 255));
+        drawContours(drawing,
+                     g_vContours,
+                     i,
+                     color,
+                     1,
+                     LINE_AA);
+        drawContours(drawing,
+                     hull,
+                     i,
+                     color,
+                     1,
+                     LINE_AA);
+    }
+    cout << "count = " << g_vContours.size() << endl
+         << "hull  = " << hull.size() << endl;
+
+    imshow(WINDOW_NAME2, drawing);
+}
+
 int main( int argc, char** argv )
 {
+    g_srcImage = imread("../converx.jpg", 1);
+
+    cvtColor(g_srcImage, g_grayImage, COLOR_BGR2GRAY);
     namedWindow(WINDOW_NAME1);
-    Mat image(600, 600, CV_8UC3);
-    RNG rng = theRNG();
+    imshow(WINDOW_NAME1, g_srcImage);
 
-    while (1) {
-        // 参数设置
-        char key;
-        int  count = (unsigned)rng % 100 + 3;
-        vector<Point> points;
+    createTrackbar("阀值", WINDOW_NAME1, &g_nThresh, g_maxThresh, on_ThreshChange);
 
-        for (int i = 0; i < count; ++i) {
-            Point point;
-            point.x = rng.uniform(image.cols / 4, image.rows * 3 / 4);
-            point.y = rng.uniform(image.rows / 4, image.rows * 3 / 4);
-            points.push_back(point);
-        }
-
-        // 检测凸包
-        vector<int> hull;
-        // 描述：寻找凸包点
-        // 第一个参数： 输入二维点集
-        // 第二个参数： 输出参数，后面找到凸包
-        // 第三个参数： 凸包的方向，为真时，顺时针方向，
-        // 第四个参数： 操作标识符，默认值为true，为真时，返回凸包各点的指数
-        convexHull(Mat(points),
-                   hull,
-                   true,
-                   true);
-
-        image = Scalar::all(0);
-        for (int i = 0; i < count; ++i) {
-            circle(image,
-                   points[i],
-                   3,
-                   Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255)),
-                   FILLED,
-                   LINE_AA);
-        }
-
-        // 准备绘制凸边的参数
-        int hullcount = (int)hull.size();
-        Point point0 = points[hull[hullcount - 1]];
-
-        // 绘制凸边
-        for (int i = 0; i < hullcount; ++i) {
-            Point point = points[hull[i]];
-            line(image, point0, point, Scalar(255, 255, 255), 2, LINE_AA);
-            point0 = point;
-        }
-        cout << "hullcount: " << hullcount << endl;
-
-        // 显示凸边
-        imshow(WINDOW_NAME1, image);
-
-        // 退出
-        key = (char)waitKey();
-        if (key == 27 || key == 'q' || key == 'Q') {
-            break;
-        }
-    }
+    on_ThreshChange(g_nThresh, NULL);
 
     waitKey(0);
 
