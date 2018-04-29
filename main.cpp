7b584fac2a531a8fad40e3155f9de491f79f0a15
Wang Sheng <15903016222@163.com>
2018-04-29 21:44:54 +0800

    4-60, 图像修复操作inpaint(),示例

--- main.cpp
+++ main.cpp
@@ -1,109 +1,136 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
+//--------------------------------------【程序说明】-------------------------------------------
+//		程序说明：《OpenCV3编程入门》OpenCV2版书本配套示例程序78
+//		程序描述：图像修补示例
+//		开发测试所用操作系统： Windows 7 64bit
+//		开发测试所用IDE版本：Visual Studio 2010
+//		开发测试所用OpenCV版本：	3.0 beta
+//		2014年11月 Created by @浅墨_毛星云
+//		2014年12月 Revised by @浅墨_毛星云
+//------------------------------------------------------------------------------------------------
 
+
+
+//---------------------------------【头文件、命名空间包含部分】----------------------------
+//		描述：包含程序所使用的头文件和命名空间
+//------------------------------------------------------------------------------------------------
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include <iostream>
 using namespace cv;
 using namespace std;
 
-#define WINDOW_NAME1 "【原始图】"
-#define WINDOW_NAME2 "【效果图】"
 
-// 定义全局变量信息
-Mat g_srcImage;
-Mat g_grayImage;
-int g_nThresh = 50;     // 阀值
-int g_nMaxThresh = 255; // 最大阀值
-RNG g_rng (12345);
-Mat g_cannyMat_output;
-vector<vector<Point> > g_vContours;
-vector<Vec4i> g_vHierarchy;
+//-----------------------------------【宏定义部分】--------------------------------------------
+//  描述：定义一些辅助宏
+//----------------------------------------------------------------------------------------------
+#define WINDOW_NAME0 "【原始图参考】"        //为窗口标题定义的宏
+#define WINDOW_NAME1 "【原始图】"        //为窗口标题定义的宏
+#define WINDOW_NAME2 "【修补后的效果图】"        //为窗口标题定义的宏
 
-// 回调函数
-void on_ContoursChange(int , void *) {
-    // 使用Canndy检测边缘
-    Canny( g_grayImage,
-           g_cannyMat_output,
-           g_nThresh,
-           g_nThresh << 1,
-           3 );
 
-    // 找到轮廓
-    findContours( g_cannyMat_output,
-                  g_vContours,
-                  g_vHierarchy,
-                  RETR_TREE,
-                  CHAIN_APPROX_SIMPLE,
-                  Point(0, 0) );
+//-----------------------------------【全局变量声明部分】--------------------------------------
+//          描述：全局变量声明
+//-----------------------------------------------------------------------------------------------
+Mat srcImage0,srcImage1, inpaintMask;
+Point previousPoint(-1,-1);//原来的点坐标
 
-    // 计算矩
-    vector<Moments> mu(g_vContours.size() );
-    for(unsigned int i = 0; i < g_vContours.size(); i++ ) {
-        mu[i] = moments( g_vContours[i], false );
-    }
 
-    //  计算中心矩
-    vector<Point2f> mc( g_vContours.size() );
-    for( unsigned int i = 0; i < g_vContours.size(); i++ ) {
-        mc[i] = Point2f( static_cast<float>(mu[i].m10/mu[i].m00),
-                         static_cast<float>(mu[i].m01/mu[i].m00));
-    }
+//-----------------------------------【ShowHelpText( )函数】----------------------------------
+//          描述：输出一些帮助信息
+//----------------------------------------------------------------------------------------------
+static void ShowHelpText( )
+{
+    //输出欢迎信息和OpenCV版本
+    printf("\n\n\t\t\t非常感谢购买《OpenCV3编程入门》一书！\n");
+    printf("\n\n\t\t\t此为本书OpenCV3版的第78个配套示例程序\n");
+    printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION );
+    printf("\n\n  ----------------------------------------------------------------------------\n");
 
-    // 绘制轮廓
-    Mat drawing = Mat::zeros( g_cannyMat_output.size(), CV_8UC3 );
-    for( unsigned int i = 0; i< g_vContours.size(); i++ ) {
-        Scalar color = Scalar( g_rng.uniform(0, 255),
-                               g_rng.uniform(0, 255),
-                               g_rng.uniform(0, 255) );//随机生成颜色值
-        drawContours( drawing,
-                      g_vContours,
-                      i,
-                      color,
-                      2,
-                      8,
-                      g_vHierarchy,
-                      0,
-                      Point() );//绘制外层和内层轮廓
-        circle( drawing,
-                mc[i],
-                4,
-                color,
-                -1,
-                8,
-                0 );;//绘制圆
-    }
-
-    // 显示到窗口中
-    namedWindow( WINDOW_NAME2, WINDOW_AUTOSIZE );
-    imshow( WINDOW_NAME2, drawing );
-
-    // 通过m00计算轮廓面积并且和OpenCV函数比较
-    printf("\t 输出内容: 面积和轮廓长度\n");
-    for(unsigned  int i = 0; i< g_vContours.size(); i++ )
-    {
-        printf(" >通过m00计算出轮廓[%d]的面积: (M_00) = %.2f \n OpenCV函数计算出的面积=%.2f , 长度: %.2f \n\n", i, mu[i].m00, contourArea(g_vContours[i]), arcLength( g_vContours[i], true ) );
-        Scalar color = Scalar( g_rng.uniform(0, 255), g_rng.uniform(0,255), g_rng.uniform(0,255) );
-        drawContours( drawing, g_vContours, i, color, 2, 8, g_vHierarchy, 0, Point() );
-        circle( drawing, mc[i], 4, color, -1, 8, 0 );
-    }
-
+    //输出一些帮助信息
+    printf("\n\n\n\t欢迎来到【图像修复】示例程序~\n");
+    printf(  "\n\t请在进行图像修复操作之前，在【原始图】窗口中进行适量的绘制"
+        "\n\n\t按键操作说明: \n\n"
+        "\t\t【鼠标左键】-在图像上绘制白色线条\n\n"
+        "\t\t键盘按键【ESC】- 退出程序\n\n"
+        "\t\t键盘按键【1】或【SPACE】-进行图像修复操作 \n\n"   );
 }
 
+
+//-----------------------------------【On_Mouse( )函数】--------------------------------
+//          描述：响应鼠标消息的回调函数
+//----------------------------------------------------------------------------------------------
+static void On_Mouse( int event, int x, int y, int flags, void* )
+{
+    //鼠标左键弹起消息
+    if( event == EVENT_LBUTTONUP || !(flags & EVENT_FLAG_LBUTTON) )
+        previousPoint = Point(-1,-1);
+    //鼠标左键按下消息
+    else if( event == EVENT_LBUTTONDOWN )
+        previousPoint = Point(x,y);
+    //鼠标按下并移动，进行绘制
+    else if( event == EVENT_MOUSEMOVE && (flags & EVENT_FLAG_LBUTTON) )
+    {
+        Point pt(x,y);
+        if( previousPoint.x < 0 )
+            previousPoint = pt;
+        //绘制白色线条
+        line( inpaintMask, previousPoint, pt, Scalar::all(255), 5, 8, 0 );
+        line( srcImage1, previousPoint, pt, Scalar::all(255), 5, 8, 0 );
+        previousPoint = pt;
+        imshow(WINDOW_NAME1, srcImage1);
+    }
+}
+
+
+//--------------------------------------【main( )函数】-----------------------------------------
+//          描述：控制台应用程序的入口函数，我们的程序从这里开始执行
+//-----------------------------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
-    g_srcImage = imread("../moments.jpg", 1);
-    imshow(WINDOW_NAME1, g_srcImage);
+    //显示帮助文字
+    ShowHelpText();
 
-    cvtColor(g_srcImage, g_grayImage, COLOR_BGR2GRAY);
-    // 平滑操作
-    blur(g_grayImage, g_grayImage, Size(3, 3));
+    //载入原始图并进行掩膜的初始化
+    Mat srcImage = imread("../water.jpg", -1);
+    if(!srcImage.data ) { printf("读取图片错误，请确定目录下是否有imread函数指定图片存在~！ \n"); return false; }
+    srcImage0 = srcImage.clone();
+    srcImage1 = srcImage.clone();
+    inpaintMask = Mat::zeros(srcImage1.size(), CV_8U);
 
-    namedWindow(WINDOW_NAME2);
-    createTrackbar("阀值", WINDOW_NAME1, &g_nThresh, g_nMaxThresh, on_ContoursChange);
+    //显示原始图参考
+    imshow(WINDOW_NAME0, srcImage0);
+    //显示原始图
+    imshow(WINDOW_NAME1, srcImage1);
+    //设置鼠标回调消息
+    setMouseCallback( WINDOW_NAME1, On_Mouse, 0 );
 
-    on_ContoursChange(g_nThresh, NULL);
+    //轮询按键，根据不同的按键进行处理
+    while (1)
+    {
+        //获取按键键值
+        char c = (char)waitKey();
 
-    waitKey();
+        //键值为ESC，程序退出
+        if( c == 27 )
+            break;
+
+        //键值为2，恢复成原始图像
+        if( c == '2' )
+        {
+            inpaintMask = Scalar::all(0);
+            srcImage.copyTo(srcImage1);
+            imshow(WINDOW_NAME1, srcImage1);
+        }
+
+        //键值为1或者空格，进行图像修补操作
+        if( c == '1' || c == ' ' )
+        {
+            Mat inpaintedImage;
+            inpaint(srcImage1, inpaintMask, inpaintedImage, 3, INPAINT_TELEA);
+            imshow(WINDOW_NAME2, inpaintedImage);
+        }
+    }
 
     return 0;
 }
