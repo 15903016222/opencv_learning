b11166eccae69eebc819fbc61cb9ceb39f1e710d
Wang Sheng <15903016222@163.com>
2018-04-30 10:31:26 +0800

    4-61,绘制直方图操作calcHist()，示例

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
-//--------------------------------------【程序说明】-------------------------------------------
-//		程序说明：《OpenCV3编程入门》OpenCV2版书本配套示例程序78
-//		程序描述：图像修补示例
-//		开发测试所用操作系统： Windows 7 64bit
-//		开发测试所用IDE版本：Visual Studio 2010
-//		开发测试所用OpenCV版本：	3.0 beta
-//		2014年11月 Created by @浅墨_毛星云
-//		2014年12月 Revised by @浅墨_毛星云
-//------------------------------------------------------------------------------------------------
-
-
-
-//---------------------------------【头文件、命名空间包含部分】----------------------------
-//		描述：包含程序所使用的头文件和命名空间
-//------------------------------------------------------------------------------------------------
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/photo/photo.hpp"
@@ -20,117 +5,82 @@
 using namespace cv;
 using namespace std;
 
+#define WINDOW_NAME1 "【原始图】"
+#define WINDOW_NAME2 "【效果图】"
 
-//-----------------------------------【宏定义部分】--------------------------------------------
-//  描述：定义一些辅助宏
-//----------------------------------------------------------------------------------------------
-#define WINDOW_NAME0 "【原始图参考】"        //为窗口标题定义的宏
-#define WINDOW_NAME1 "【原始图】"        //为窗口标题定义的宏
-#define WINDOW_NAME2 "【修补后的效果图】"        //为窗口标题定义的宏
-
-
-//-----------------------------------【全局变量声明部分】--------------------------------------
-//          描述：全局变量声明
-//-----------------------------------------------------------------------------------------------
-Mat srcImage0,srcImage1, inpaintMask;
-Point previousPoint(-1,-1);//原来的点坐标
-
-
-//-----------------------------------【ShowHelpText( )函数】----------------------------------
-//          描述：输出一些帮助信息
-//----------------------------------------------------------------------------------------------
-static void ShowHelpText( )
-{
-    //输出欢迎信息和OpenCV版本
-    printf("\n\n\t\t\t非常感谢购买《OpenCV3编程入门》一书！\n");
-    printf("\n\n\t\t\t此为本书OpenCV3版的第78个配套示例程序\n");
-    printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION );
-    printf("\n\n  ----------------------------------------------------------------------------\n");
-
-    //输出一些帮助信息
-    printf("\n\n\n\t欢迎来到【图像修复】示例程序~\n");
-    printf(  "\n\t请在进行图像修复操作之前，在【原始图】窗口中进行适量的绘制"
-        "\n\n\t按键操作说明: \n\n"
-        "\t\t【鼠标左键】-在图像上绘制白色线条\n\n"
-        "\t\t键盘按键【ESC】- 退出程序\n\n"
-        "\t\t键盘按键【1】或【SPACE】-进行图像修复操作 \n\n"   );
-}
-
-
-//-----------------------------------【On_Mouse( )函数】--------------------------------
-//          描述：响应鼠标消息的回调函数
-//----------------------------------------------------------------------------------------------
-static void On_Mouse( int event, int x, int y, int flags, void* )
-{
-    //鼠标左键弹起消息
-    if( event == EVENT_LBUTTONUP || !(flags & EVENT_FLAG_LBUTTON) )
-        previousPoint = Point(-1,-1);
-    //鼠标左键按下消息
-    else if( event == EVENT_LBUTTONDOWN )
-        previousPoint = Point(x,y);
-    //鼠标按下并移动，进行绘制
-    else if( event == EVENT_MOUSEMOVE && (flags & EVENT_FLAG_LBUTTON) )
-    {
-        Point pt(x,y);
-        if( previousPoint.x < 0 )
-            previousPoint = pt;
-        //绘制白色线条
-        line( inpaintMask, previousPoint, pt, Scalar::all(255), 5, 8, 0 );
-        line( srcImage1, previousPoint, pt, Scalar::all(255), 5, 8, 0 );
-        previousPoint = pt;
-        imshow(WINDOW_NAME1, srcImage1);
-    }
-}
-
-
-//--------------------------------------【main( )函数】-----------------------------------------
-//          描述：控制台应用程序的入口函数，我们的程序从这里开始执行
-//-----------------------------------------------------------------------------------------------
 int main( int argc, char** argv )
 {
-    //显示帮助文字
-    ShowHelpText();
+    Mat srcImage, hsvImage;
 
-    //载入原始图并进行掩膜的初始化
-    Mat srcImage = imread("../water.jpg", -1);
-    if(!srcImage.data ) { printf("读取图片错误，请确定目录下是否有imread函数指定图片存在~！ \n"); return false; }
-    srcImage0 = srcImage.clone();
-    srcImage1 = srcImage.clone();
-    inpaintMask = Mat::zeros(srcImage1.size(), CV_8U);
+    srcImage = imread("../cup.jpg");
+    cvtColor(srcImage, hsvImage, COLOR_BGR2HSV);
+    imshow(WINDOW_NAME1, srcImage);
 
-    //显示原始图参考
-    imshow(WINDOW_NAME0, srcImage0);
-    //显示原始图
-    imshow(WINDOW_NAME1, srcImage1);
-    //设置鼠标回调消息
-    setMouseCallback( WINDOW_NAME1, On_Mouse, 0 );
+    // 参数准备
+    int hueBinNum = 30;
+    int saturationBinNum = 32;
+    int histSize[] = {hueBinNum, saturationBinNum};
+    float hueRanges[] = {0, 180};
+    float saturationRanges[] = {0, 256};
+    const float *ranges[] = {hueRanges, saturationRanges};
+    MatND dstHist;
+    int channels[] = {0, 1};
 
-    //轮询按键，根据不同的按键进行处理
-    while (1)
-    {
-        //获取按键键值
-        char c = (char)waitKey();
+    // 正式调用calcHist，进行直方图计算
+    // 描述： 计算直方图
+    // 第一个参数： 输入数组(或数组集)
+    // 第二个参数： 输入数组的个数
+    // 第三个参数： 需要统计的通道索引
+    // 第四个参数： 可选的操作掩码
+    // 第五个参数： 输出直方图目标
+    // 第六个参数： 需要计算的直方图纬度
+    // 第七个参数： 存放每个纬度的直方图尺寸的数组
+    // 第八个参数： 表示每一个纬度数组
+    // 第九个参数： 指示直方图是否均匀的标示符
+    // 第十个参数： 累计标示符
+    calcHist(&hsvImage,
+             1,
+             channels,
+             Mat(),
+             dstHist,
+             2,
+             histSize,
+             ranges,
+             true,
+             false);
 
-        //键值为ESC，程序退出
-        if( c == 27 )
-            break;
+    // 为绘制直方图准备参数
+    double maxValue = 0;
+    // 描述： 在数组中找到全局的最小值和最大值
+    // 第一个参数： 输入单通道阵列
+    // 第二个参数： 返回最小值的指针
+    // 第三个参数： 返回最大值指针
+    // 第四个参数： 返回最小位置的指针
+    // 第五个参数： 返回最大位置的指针
+    // 第六个参数： 用于选择子阵列的可选掩膜
+    minMaxLoc(dstHist, 0, &maxValue, 0, 0);
+    int scale = 10;
+    Mat histImg = Mat::zeros(saturationBinNum * scale, hueBinNum * 10, CV_8UC3);
 
-        //键值为2，恢复成原始图像
-        if( c == '2' )
-        {
-            inpaintMask = Scalar::all(0);
-            srcImage.copyTo(srcImage1);
-            imshow(WINDOW_NAME1, srcImage1);
-        }
+    // 双层循环，进行直方图的绘制
+    for (int hue = 0; hue < hueBinNum; ++hue) {
+        for (int saturation = 0; saturation < saturationBinNum; ++saturation) {
+            float binValue = dstHist.at<float>(hue, saturation);
+            int intensity = cvRound(binValue * 255 / maxValue);
 
-        //键值为1或者空格，进行图像修补操作
-        if( c == '1' || c == ' ' )
-        {
-            Mat inpaintedImage;
-            inpaint(srcImage1, inpaintMask, inpaintedImage, 3, INPAINT_TELEA);
-            imshow(WINDOW_NAME2, inpaintedImage);
+            // 正式绘制
+            rectangle(histImg,
+                      Point(hue * scale, saturation * scale),
+                      Point((hue + 1) * scale - 1, (saturation + 1) * scale - 1),
+                      Scalar::all(intensity),
+                      FILLED);
         }
     }
+
+    // 显示效果图
+    imshow(WINDOW_NAME2, histImg);
+
+    waitKey(0);
 
     return 0;
 }
