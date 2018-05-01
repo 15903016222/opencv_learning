a03210ca609b67146b9b5272054dd1f05e81322c
Wang Sheng <15903016222@163.com>
2018-05-01 17:12:08 +0800

    4-62,投影操作

--- main.cpp
+++ main.cpp
@@ -1,86 +1,125 @@
-#include "opencv2/highgui/highgui.hpp"
+//--------------------------------------【程序说明】-------------------------------------------
+//		程序说明：《OpenCV3编程入门》OpenCV2版书本配套示例程序83
+//		程序描述：反向投影示例程序
+//		开发测试所用操作系统： Windows 7 64bit
+//		开发测试所用IDE版本：Visual Studio 2010
+//		开发测试所用OpenCV版本：	3.0 beta
+//		2014年11月 Created by @浅墨_毛星云
+//		2014年12月 Revised by @浅墨_毛星云
+//------------------------------------------------------------------------------------------------
+
+
+
+//---------------------------------【头文件、命名空间包含部分】----------------------------
+//		描述：包含程序所使用的头文件和命名空间
+//------------------------------------------------------------------------------------------------
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/photo/photo.hpp"
-#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
 using namespace cv;
-using namespace std;
 
-#define WINDOW_NAME1 "【原始图】"
-#define WINDOW_NAME2 "【效果图】"
 
-int main( int argc, char** argv )
+//-----------------------------------【宏定义部分】--------------------------------------------
+//  描述：定义一些辅助宏
+//------------------------------------------------------------------------------------------------
+#define WINDOW_NAME1 "【原始图】"        //为窗口标题定义的宏
+
+
+//-----------------------------------【全局变量声明部分】--------------------------------------
+//          描述：全局变量声明
+//-----------------------------------------------------------------------------------------------
+Mat g_srcImage; Mat g_hsvImage; Mat g_hueImage;
+int g_bins = 30;//直方图组距
+
+//-----------------------------------【全局函数声明部分】--------------------------------------
+//          描述：全局函数声明
+//-----------------------------------------------------------------------------------------------
+static void ShowHelpText();
+void on_BinChange(int, void* );
+
+//--------------------------------------【main( )函数】-----------------------------------------
+//          描述：控制台应用程序的入口函数，我们的程序从这里开始执行
+//-----------------------------------------------------------------------------------------------
+int main( )
 {
-    Mat srcImage, hsvImage;
+    //【0】改变console字体颜色
+    system("color 6F");
 
-    srcImage = imread("../cup.jpg");
-    cvtColor(srcImage, hsvImage, COLOR_BGR2HSV);
-    imshow(WINDOW_NAME1, srcImage);
+    //【0】显示帮助文字
+    ShowHelpText();
 
-    // 参数准备
-    int hueBinNum = 30;
-    int saturationBinNum = 32;
-    int histSize[] = {hueBinNum, saturationBinNum};
-    float hueRanges[] = {0, 180};
-    float saturationRanges[] = {0, 256};
-    const float *ranges[] = {hueRanges, saturationRanges};
-    MatND dstHist;
-    int channels[] = {0, 1};
+    //【1】读取源图像，并转换到 HSV 空间
+    g_srcImage = imread( "../hand.jpg", 1 );
+    if(!g_srcImage.data ) { printf("读取图片错误，请确定目录下是否有imread函数指定图片存在~！ \n"); return false; }
+    cvtColor( g_srcImage, g_hsvImage, COLOR_BGR2HSV );
 
-    // 正式调用calcHist，进行直方图计算
-    // 描述： 计算直方图
-    // 第一个参数： 输入数组(或数组集)
-    // 第二个参数： 输入数组的个数
-    // 第三个参数： 需要统计的通道索引
-    // 第四个参数： 可选的操作掩码
-    // 第五个参数： 输出直方图目标
-    // 第六个参数： 需要计算的直方图纬度
-    // 第七个参数： 存放每个纬度的直方图尺寸的数组
-    // 第八个参数： 表示每一个纬度数组
-    // 第九个参数： 指示直方图是否均匀的标示符
-    // 第十个参数： 累计标示符
-    calcHist(&hsvImage,
-             1,
-             channels,
-             Mat(),
-             dstHist,
-             2,
-             histSize,
-             ranges,
-             true,
-             false);
+    //【2】分离 Hue 色调通道
+    g_hueImage.create( g_hsvImage.size(), g_hsvImage.depth() );
+    int ch[ ] = { 0, 0 };
+    mixChannels( &g_hsvImage, 1, &g_hueImage, 1, ch, 1 );
 
-    // 为绘制直方图准备参数
-    double maxValue = 0;
-    // 描述： 在数组中找到全局的最小值和最大值
-    // 第一个参数： 输入单通道阵列
-    // 第二个参数： 返回最小值的指针
-    // 第三个参数： 返回最大值指针
-    // 第四个参数： 返回最小位置的指针
-    // 第五个参数： 返回最大位置的指针
-    // 第六个参数： 用于选择子阵列的可选掩膜
-    minMaxLoc(dstHist, 0, &maxValue, 0, 0);
-    int scale = 10;
-    Mat histImg = Mat::zeros(saturationBinNum * scale, hueBinNum * 10, CV_8UC3);
+    //【3】创建 Trackbar 来输入bin的数目
+    namedWindow( WINDOW_NAME1 , WINDOW_AUTOSIZE );
+    createTrackbar("色调组距 ", WINDOW_NAME1 , &g_bins, 180, on_BinChange );
+    on_BinChange(0, 0);//进行一次初始化
 
-    // 双层循环，进行直方图的绘制
-    for (int hue = 0; hue < hueBinNum; ++hue) {
-        for (int saturation = 0; saturation < saturationBinNum; ++saturation) {
-            float binValue = dstHist.at<float>(hue, saturation);
-            int intensity = cvRound(binValue * 255 / maxValue);
+    //【4】显示效果图
+    imshow( WINDOW_NAME1 , g_srcImage );
 
-            // 正式绘制
-            rectangle(histImg,
-                      Point(hue * scale, saturation * scale),
-                      Point((hue + 1) * scale - 1, (saturation + 1) * scale - 1),
-                      Scalar::all(intensity),
-                      FILLED);
-        }
-    }
-
-    // 显示效果图
-    imshow(WINDOW_NAME2, histImg);
-
+    // 等待用户按键
     waitKey(0);
-
     return 0;
+}
+
+
+//-----------------------------------【on_HoughLines( )函数】--------------------------------
+//          描述：响应滑动条移动消息的回调函数
+//---------------------------------------------------------------------------------------------
+void on_BinChange(int, void* )
+{
+    //【1】参数准备
+    MatND hist;
+    int histSize = MAX( g_bins, 2 );
+    float hue_range[] = { 0, 180 };
+    const float* ranges = { hue_range };
+
+    //【2】计算直方图并归一化
+    calcHist( &g_hueImage, 1, 0, Mat(), hist, 1, &histSize, &ranges, true, false );
+    normalize( hist, hist, 0, 255, NORM_MINMAX, -1, Mat() );
+
+    //【3】计算反向投影
+    MatND backproj;
+    calcBackProject( &g_hueImage, 1, 0, hist, backproj, &ranges, 1, true );
+
+    //【4】显示反向投影
+    imshow( "反向投影图", backproj );
+
+    //【5】绘制直方图的参数准备
+    int w = 400; int h = 400;
+    int bin_w = cvRound( (double) w / histSize );
+    Mat histImg = Mat::zeros( w, h, CV_8UC3 );
+
+    //【6】绘制直方图
+    for( int i = 0; i < g_bins; i ++ )
+    { rectangle( histImg, Point( i*bin_w, h ), Point( (i+1)*bin_w, h - cvRound( hist.at<float>(i)*h/255.0 ) ), Scalar( 100, 123, 255 ), -1 ); }
+
+    //【7】显示直方图窗口
+    imshow( "直方图", histImg );
+}
+
+
+//-----------------------------------【ShowHelpText( )函数】----------------------------------
+//          描述：输出一些帮助信息
+//----------------------------------------------------------------------------------------------
+static void ShowHelpText()
+{
+    //输出欢迎信息和OpenCV版本
+    printf("\n\n\t\t\t非常感谢购买《OpenCV3编程入门》一书！\n");
+    printf("\n\n\t\t\t此为本书OpenCV3版的第83个配套示例程序\n");
+    printf("\n\n\t\t\t   当前使用的OpenCV版本为：" CV_VERSION );
+    printf("\n\n  ----------------------------------------------------------------------------\n");
+
+    //输出一些帮助信息
+    printf("\n\n\t欢迎来到【反向投影】示例程序\n\n");
+    printf("\n\t请调整滑动条观察图像效果\n\n");
+
 }
