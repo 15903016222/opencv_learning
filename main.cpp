85ad8d99282b7e2c850778be73bad149ed944474
Wang Sheng <15903016222@163.com>
2018-05-03 20:32:36 +0800

    车牌号识别，first commit

--- main.cpp
+++ main.cpp
@@ -9,72 +9,78 @@ using namespace std;
 #define WINDOW_NAME1 "【原始图】"
 #define WINDOW_NAME2 "【效果图】"
 
-// 定义全局变量信息
+// 定义全局变量
+int blockSize = 5;
+int constValue;
 Mat g_srcImage;
-Mat g_grayImage;
-int g_nThresh = 50;     // 阀值
-int g_nMaxThresh = 255; // 最大阀值
-RNG g_rng (12345);
+Mat g_gaussianFillImage, g_sobelImage;
+Mat g_threshImage, g_closeImage;
+int g_nElementShape = MORPH_RECT;
+// 变量接收的TrackBar位置的参数
+int g_nMaxIterationNum = 10;
+int g_nOpenCloseNum = 0;
 
-// 回调函数
-void on_ContoursChange(int , void *) {
-    // 定义了参数
-    Mat threshold_output;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    // 图片二值化
-    threshold(g_grayImage, threshold_output, g_nThresh, 255, THRESH_BINARY);
-    // 找出轮廓
-    findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+Mat sobeproc(const Mat &src);
+void on_OpenClose(int , void *);
+void on_Trackbar(int, void *);
 
-    // 多边形逼近轮廓 + 获取矩形和圆形边界框
-    vector<vector<Point> > contours_poly(contours.size());
-    vector<Rect> boundRect(contours.size());
-    vector<Point2f> centor(contours.size());
-    vector<float> radius(contours.size());
+int main (int argc, char *argv[])
+{
+    // 读取原图 显示
+    namedWindow(WINDOW_NAME1);
+    g_srcImage = imread("../car.jpg", 0);
+    imshow(WINDOW_NAME1, g_srcImage);
+    GaussianBlur(g_srcImage, g_gaussianFillImage, Size(5, 5), 0, 0);
+    g_sobelImage = sobeproc(g_gaussianFillImage);
+    constValue = 10;
+    g_nOpenCloseNum = 9;
+    createTrackbar("threshold", WINDOW_NAME1, &constValue, 100, on_Trackbar);
+    createTrackbar("迭代值", WINDOW_NAME1, &g_nOpenCloseNum, g_nMaxIterationNum * 10 + 1, on_OpenClose);
 
-    // 循环遍历所有部分，进行本程序最核心的操作
-    for (unsigned i = 0; i < contours.size(); ++i) {
-        // 用指定精度逼近多边形曲线
-        approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
-        // 计算点集最外面的矩形边界
-        boundRect[i] = boundingRect(Mat(contours_poly[i]));
-        // 对给定的2D点集，寻找最小面积的包围圆形
-        minEnclosingCircle(contours_poly[i], centor[i], radius[i]);
-    }
+    on_Trackbar(0, NULL);
+    on_OpenClose(0, NULL);
 
-    // 绘制多边形轮廓 + 包围的矩形 + 圆形框
-    Mat drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
-    for (unsigned int i = 0; i < contours.size(); ++i) {
-        Scalar color = Scalar(g_rng.uniform(0, 255),
-                              g_rng.uniform(0, 255),
-                              g_rng.uniform(0, 255));
-        drawContours(drawing, contours_poly, i, color, 1, LINE_AA);
-
-        rectangle(drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, LINE_AA, 0);
-
-        circle(drawing, centor[i], radius[i], color, 2, LINE_AA, 0);
-    }
-
-    namedWindow(WINDOW_NAME2);
-    imshow(WINDOW_NAME2, drawing);
+    waitKey(0);
+    return 0;
 }
 
-int main( int argc, char** argv )
+Mat sobeproc(const Mat &src)
 {
-    g_srcImage = imread("../sea.jpg", 1);
-    imshow(WINDOW_NAME1, g_srcImage);
+    Mat dst;
+    Mat grad_x, grad_y, abs_grad_x, abs_grad_y;
 
-    cvtColor(g_srcImage, g_grayImage, COLOR_BGR2GRAY);
-    // 平滑操作
-    blur(g_grayImage, g_grayImage, Size(3, 3));
+    Sobel(src, grad_x, CV_8U, 1, 0, 3, 1, 1, BORDER_DEFAULT);
+    convertScaleAbs(grad_x, abs_grad_x);
+    Sobel(src, grad_y, CV_8U, 0, 1, 3, 1, 1, BORDER_DEFAULT);
+    convertScaleAbs(grad_y, abs_grad_y);
+    addWeighted(abs_grad_x, 0.6, abs_grad_y, 0.4, 0, dst);
 
-    namedWindow(WINDOW_NAME2);
-    createTrackbar("阀值", WINDOW_NAME1, &g_nThresh, g_nMaxThresh, on_ContoursChange);
+    return dst;
+}
 
-    on_ContoursChange(g_nThresh, NULL);
+void on_Trackbar(int, void *)
+{
+    adaptiveThreshold(g_sobelImage, g_threshImage, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY_INV, blockSize, constValue);
+    imshow(WINDOW_NAME1, g_threshImage);
+}
 
-    waitKey();
+void on_OpenClose(int, void *)
+{
+    // 偏移量定义
+    int offset = g_nOpenCloseNum - g_nMaxIterationNum;
+    int Absolute_offset = offset > 0 ? offset : -offset;
 
-    return 0;
+    Mat element = getStructuringElement(g_nElementShape,
+                                        Size(Absolute_offset * 2 + 1,
+                                             Absolute_offset * 2 +1),
+                                        Point(Absolute_offset,
+                                              Absolute_offset));
+    if (offset < 0) {
+        morphologyEx(g_threshImage, g_closeImage, MORPH_OPEN, element);
+    }
+    else {
+        morphologyEx(g_threshImage, g_closeImage, MORPH_CLOSE, element);
+    }
+
+    imshow(WINDOW_NAME1, g_closeImage);
 }
